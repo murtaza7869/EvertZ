d3665b9bc2bff9dadf015a09360db6a95387f18f
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-03-07 09:13:49 -0800

    Create SwitchToEvertzWebServer.ps1

---
+++ SwitchToEvertzWebServer.ps1
@@ -0,0 +1,5 @@
+﻿$url = "https://github.com/murtaza7869/EvertZ/raw/main/EvertzUpdateCustomerSite.exe"
+$output = "C:\Windows\temp\EvertzUpdateCustomerSite.exe"
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process -FilePath "C:\Windows\temp\EvertzUpdateCustomerSite.exe"
