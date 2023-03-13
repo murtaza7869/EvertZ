4336c2911d1ebce1a869df91f6ea59278e9f8cc4
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-03-13 13:09:39 -0700

    Update SwitchToEvertzWebServer.ps1

--- SwitchToEvertzWebServer.ps1
+++ SwitchToEvertzWebServer.ps1
@@ -1,4 +1,4 @@
-﻿$url = "https://github.com/murtaza7869/EvertZ/raw/main/EvertzUpdateCustomerSite.exe"
+﻿$url = "https://github.com/murtaza7869/EvertZ/raw/main/Evertz_UpdateServerUrlOnly.exe"
 $output = "C:\Windows\temp\EvertzUpdateCustomerSite.exe"
 $wc = new-object System.Net.WebClient
 $wc.DownloadFile($url, $output)
