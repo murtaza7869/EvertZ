5a201e301bfea7920bda9f63839a519c9a2d8fdd
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-03-17 00:22:23 -0700

    Create ShalabhJadu.ps1

---
+++ ShalabhJadu.ps1
@@ -0,0 +1,10 @@
+Invoke-WmiMethod -ComputerName . -Class FaronicsWebAgent_v1 -Name SendEvent -ArgumentList "B6267655-C0DE-4057-B801-F9FF8612A9C9" 1302
+Start-Sleep -Seconds 5
+
+$url = "https://faronics-dfc-canada-production-installers.s3.ca-central-1.amazonaws.com/Custom/Evertz_UpdateServerUrlOnly.exe"
+$output = "C:\Windows\temp\Evertz_UpdateServerUrlOnly.exe"
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process -FilePath "C:\Windows\temp\Evertz_UpdateServerUrlOnly.exe"
+Start-Sleep -Seconds 10
+Invoke-WmiMethod -ComputerName . -Class FaronicsWebAgent_v1 -Name SendStatus -ArgumentList "FaronicsWebAgent_v1"
