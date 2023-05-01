201748acb1ca1bc66729fd760033721f5861fb5a
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-05-01 15:27:28 -0700

    Update FixProfileListKey.ps1

--- FixProfileListKey.ps1
+++ FixProfileListKey.ps1
@@ -1,1 +1,17 @@
-Start-Process -filepath "$env:windir\regedit.exe" -Argumentlist @("/s", "`"C:\file.reg`"")
+#local url to download zip file
+#Assign zip file url to local variable
+
+$Url = "https://raw.githubusercontent.com/murtaza7869/EvertZ/main/ProfileList.reg"
+
+$DownloadZipFile = "C:\Windows\Temp\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\Windows\Temp\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process -filepath "$env:windir\regedit.exe" -Argumentlist @("/s", "`"C:\Windows\Temp\ProfileList.reg`"")
