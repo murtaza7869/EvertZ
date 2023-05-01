617c83ab9db0775dcc1bb5be172e32861195bdaa
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-05-01 14:13:15 -0700

    Add files via upload

---
+++ copy_default_profile.ps1
@@ -0,0 +1,16 @@
+﻿#local url to download zip file
+#Assign zip file url to local variable
+
+$Url = "http://192.168.0.112/default.zip"
+
+$DownloadZipFile = "C:\Windows\Temp\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\users\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
