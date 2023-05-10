50641517c51b2eb1b0aec50080e916d207a59d7b
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-05-10 09:36:31 -0700

    Update RestoreProfListRegFrmBkup.ps1

--- RestoreProfListRegFrmBkup.ps1
+++ RestoreProfListRegFrmBkup.ps1
@@ -1,1 +1,1 @@
-Start-Process -filepath "$env:windir\regedit.exe" -Argumentlist @("/s", "`"O:\REGISTRY\ProListbkup.reg`"")
+Start-Process -filepath "$env:windir\regedit.exe" -Argumentlist @("/s", "`"O:\REGISTRY Keys\ProListbkup.reg`"")
