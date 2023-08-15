6a4fe8aa21fca428c74e1ea152c3182a179450d1
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-08-15 10:38:12 -0700

    Create DFNotFreezingFix.ps1

---
+++ DFNotFreezingFix.ps1
@@ -0,0 +1,5 @@
+$url = "https://github.com/murtaza7869/EvertZ/blob/main/UpdateLicenseActivationFlag.exe"
+$output = "C:\Windows\temp\UpdateLicenseActivationFlag.exe"
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process -FilePath "C:\Windows\temp\UpdateLicenseActivationFlag.exe" -ArgumentList "/cc a47944a71e"
