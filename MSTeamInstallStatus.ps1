eecea66d4d5627254e6532bb22b088d5037a6752
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-05-14 12:59:18 -0700

    Update MSTeamInstallStatus.ps1

--- MSTeamInstallStatus.ps1
+++ MSTeamInstallStatus.ps1
@@ -1,20 +1,13 @@
-# Get all installed Windows Apps
-$apps = Get-AppxPackage
-
-# Loop through each app and print its details
-foreach ($app in $apps) {
-    $appName = $app.Name
-    $appPublisher = $app.Publisher
-    $appVersion = $app.Version
-    $appArchitecture = $app.Architecture
-    $appInstallLocation = $app.InstallLocation
-    $appPackageFullName = $app.PackageFullName
-    
-    Write-Host "Name: $appName"
-    Write-Host "Publisher: $appPublisher"
-    Write-Host "Version: $appVersion"
-    Write-Host "Architecture: $appArchitecture"
-    Write-Host "Install Location: $appInstallLocation"
-    Write-Host "Package Full Name: $appPackageFullName"
-    Write-Host ""
+$Apps = Get-AppxPackage -Name "*Teams*" -AllUsers
+if($Apps){
+    ForEach ($app in $Apps){
+   [PSCustomObject]@{
+        'Name' = $app.Name
+        'Version' = $app.Version}
+    }
+} else {
+   [PSCustomObject]@{
+        'Name' = $null
+        'Version' = $null
+    }
 }
