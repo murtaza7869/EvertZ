4cc20d71fc4db36a79015aaf8b7814dc30f499f5
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-05-16 13:53:15 -0700

    Add files via upload
    This script checks if Winget is isntalled, if not installs it , if outdated updates it and then downlaods and isntalls MS TEams WIndows APp

---
+++ ChecksforWingetandInstallsMSTEAMS.ps1
@@ -0,0 +1,51 @@
+﻿# Function to check if Winget is installed
+function Check-Winget {
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        Write-Output "Winget is already installed."
+    } else {
+        Write-Output "Winget is not installed. Please install Winget v1.7 or higher from https://github.com/microsoft/winget-cli/releases"
+        exit 1
+    }
+}
+
+# Function to update Winget
+function Update-Winget {
+    try {
+        winget upgrade --id Microsoft.Winget.Client --accept-source-agreements --accept-package-agreements
+        Write-Output "Winget has been updated."
+    } catch {
+        Write-Output "Failed to update Winget."
+        exit 1
+    }
+}
+
+# Function to install applications using Winget
+function Install-Apps {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]] $Apps
+    )
+
+    foreach ($App in $Apps) {
+        try {
+            winget install --id $App --silent --accept-source-agreements --accept-package-agreements
+            Write-Output "$App has been installed."
+        } catch {
+            Write-Output "Failed to install $App."
+        }
+    }
+}
+
+# Check if Winget is installed
+Check-Winget
+
+# Update Winget to the latest version
+Update-Winget
+
+# List of applications to install
+$appsToInstall = @(
+        "Microsoft.Teams"   #MSTEAMS
+)
+
+# Install applications
+Install-Apps -Apps $appsToInstall
