9c88850b2613b2a3ae2c9f8aae9c56dccc63b3d5
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-08-26 11:11:50 -0700

    Update Readxml.ps1

--- Readxml.ps1
+++ Readxml.ps1
@@ -1,8 +1,20 @@
-# Define the path to the XML file
-# $xmlFilePath = $Args
+# Check if the script was passed an argument
+if ($args.Length -eq 0) {
+    Write-Host "Usage: .\ReadXmlFile.ps1 <PathToXmlFile>"
+    exit
+}
+
+# Get the XML file path from the script's argument
+$xmlFilePath = $args[0]
+
+# Check if the file exists
+if (-Not (Test-Path $xmlFilePath)) {
+    Write-Host "File not found: $xmlFilePath"
+    exit
+}
 
 # Load the XML file
-[xml]$xmlContent = Get-Content $Args
+[xml]$xmlContent = Get-Content $xmlFilePath
 
 # Print the XML content to the console
 $xmlContent.OuterXml
