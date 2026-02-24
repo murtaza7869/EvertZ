bd0380c204bde8911cdc33582a38957ca765f672
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2026-02-24 09:32:18 -0800

    Create ReinstateFWASvcControl.ps1
    removes lock and reinstates admin access to control service.

---
+++ ReinstateFWASvcControl.ps1
@@ -0,0 +1,2 @@
+# Resets the service to the standard default SDDL
+sc.exe sdset "FWASvc" "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)"
